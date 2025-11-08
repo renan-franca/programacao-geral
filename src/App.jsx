32448e04b022726bcd8f74cd9d3301ee3752ddfd
Renan França <83841256+renan-franca@users.noreply.github.com>
2025-11-08 20:09:27 +0000

    init: vite + react (mensagem Deborah)

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '2rem',
+      textAlign: 'center',
+      background: '#fff'
+    }}>
+      <p>Deborah, eu te amo. Se gostou dessa mensagem, dê uma risadinha 💙</p>
+    </div>
+  );
+}
